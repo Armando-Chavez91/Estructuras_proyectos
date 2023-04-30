c7a0e33c11cfa05c142026242096438b352be544
Armando-Chavez91 <124794857+Armando-Chavez91@users.noreply.github.com>
2023-04-30 00:13:15 -0600

    Update Program.cs

--- LAB1/Llaboratorio1_Rafael_Chavez_1191121/Llaboratorio1_Rafael_Chavez_1191121/Program.cs
+++ LAB1/Llaboratorio1_Rafael_Chavez_1191121/Llaboratorio1_Rafael_Chavez_1191121/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.IO;
-using System.Security.Cryptography.X509Certificates;
 
 namespace InmueblesGT
 {
@@ -11,6 +10,7 @@ namespace InmueblesGT
         static void Main(string[] args)
         {
             // Leer el archivo .jsonl en la carpeta del proyecto
+
             string _path = @"D:\Universidad Rafael Landìvar\6to Ciclo 2023\Estructura de Datos I\LABORATORIO 1\labED_1\input_challenge.jsonl";
             string inputString;
             using (var reader = new StreamReader(_path))
@@ -18,29 +18,59 @@ namespace InmueblesGT
                 inputString = reader.ReadToEnd();
             }
 
-            // Dividir el archivo por cada objeto JSON
             string[] inputObjects = inputString.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
-            // Procesar cada objeto JSON
             foreach (string obj in inputObjects)
             {
-                // Deserializar el objeto JSON
                 dynamic data = JsonConvert.DeserializeObject(obj);
 
-                // Obtener los valores de input1 e input2
                 List<Dictionary<string, bool>> map = data.input1.ToObject<List<Dictionary<string, bool>>>();
                 List<string> requirements = data.input2.ToObject<List<string>>();
 
-                // Procesar la información y obtener las recomendaciones de apartamentos
-                // List<int> recommendations = GetApartmentRecommendations(map, requirements);
+                List<int> recommendations = GetApartmentRecommendations(map, requirements);
 
-                // Imprimir las recomendaciones en la consola
-                // Console.WriteLine(JsonConvert.SerializeObject(recommendations));
+                Console.WriteLine(JsonConvert.SerializeObject(recommendations));
             }
 
-            // Esperar a que el usuario presione cualquier tecla antes de salir
             Console.WriteLine("Presione cualquier tecla para salir...");
             Console.ReadKey();
         }
+
+        static List<int> GetApartmentRecommendations(List<Dictionary<string, bool>> map, List<string> requirements)
+        {
+            List<int> recommendations = new List<int>();
+            List<int> distances = new List<int>();
+
+     for (int i = 0; i < map.Count; i++)
+            {
+                Dictionary<string, bool> apartment = map[i];
+                bool meetsRequirements = true;
+
+                foreach (string requirement in requirements)
+                {
+                    if (!apartment.ContainsKey(requirement) || !apartment[requirement])
+                    {
+                        meetsRequirements = false;
+                        break;
+                    }
+                }
+
+                if (meetsRequirements)
+                {
+                    int distance = i; // Distancia es igual al índice del apartamento
+                    distances.Add(distance);
+                }
+            }
+
+           distances.Sort();
+            foreach (int distance in distances)
+            {
+                recommendations.Add(distance);
+            }
+
+            return recommendations;
+        }
     }
 }
+
+
