6791b7bb78854fa39f261500c5ade1934dc2aeae
Armando-Chavez91 <124794857+Armando-Chavez91@users.noreply.github.com>
2023-04-30 00:15:31 -0600

    Update Program.cs

--- LAB1/Llaboratorio1_Rafael_Chavez_1191121/Llaboratorio1_Rafael_Chavez_1191121/Program.cs
+++ LAB1/Llaboratorio1_Rafael_Chavez_1191121/Llaboratorio1_Rafael_Chavez_1191121/Program.cs
@@ -18,44 +18,34 @@ namespace InmueblesGT
                 inputString = reader.ReadToEnd();
             }
 
-            // Dividir el archivo por cada objeto JSON
             string[] inputObjects = inputString.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
-            // Procesar cada objeto JSON
             foreach (string obj in inputObjects)
             {
-                // Deserializar el objeto JSON
                 dynamic data = JsonConvert.DeserializeObject(obj);
 
-                // Obtener los valores de input1 e input2
                 List<Dictionary<string, bool>> map = data.input1.ToObject<List<Dictionary<string, bool>>>();
                 List<string> requirements = data.input2.ToObject<List<string>>();
 
-                // Procesar la información y obtener las recomendaciones de apartamentos
                 List<int> recommendations = GetApartmentRecommendations(map, requirements);
 
-                // Imprimir las recomendaciones en la consola
                 Console.WriteLine(JsonConvert.SerializeObject(recommendations));
             }
 
-            // Esperar a que el usuario presione cualquier tecla antes de salir
             Console.WriteLine("Presione cualquier tecla para salir...");
             Console.ReadKey();
         }
 
-        // Función que obtiene las recomendaciones de apartamentos de acuerdo con los requerimientos del cliente
         static List<int> GetApartmentRecommendations(List<Dictionary<string, bool>> map, List<string> requirements)
         {
             List<int> recommendations = new List<int>();
             List<int> distances = new List<int>();
 
-            // Recorre cada apartamento en el mapa
             for (int i = 0; i < map.Count; i++)
             {
                 Dictionary<string, bool> apartment = map[i];
                 bool meetsRequirements = true;
 
-                // Verifica si el apartamento cumple con los requerimientos del cliente
                 foreach (string requirement in requirements)
                 {
                     if (!apartment.ContainsKey(requirement) || !apartment[requirement])
@@ -65,8 +55,6 @@ namespace InmueblesGT
                     }
                 }
 
-                // Si el apartamento cumple con los requerimientos, se calcula la distancia
-                // y se agrega a la lista de recomendaciones
                 if (meetsRequirements)
                 {
                     int distance = i; // Distancia es igual al índice del apartamento
@@ -74,7 +62,6 @@ namespace InmueblesGT
                 }
             }
 
-            // Ordena la lista de recomendaciones por distancia y las agrega a la lista de recomendaciones
             distances.Sort();
             foreach (int distance in distances)
             {
